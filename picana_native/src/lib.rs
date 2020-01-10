dd98ee654fc1cb3d9dfa9a84d2a0adbd92beebde
Harry K <kituyiharry@gmail.com>
2020-01-10 20:06:50 +0300

    Async message sending

--- picana_native/src/lib.rs
+++ picana_native/src/lib.rs
@@ -595,7 +595,6 @@ pub mod picana {
     #[no_mangle]
     pub unsafe extern "C" fn silence() -> i32 {
         let picana = Arc::clone(&PICANA);
-        print!("Silencing!\n");
         let r = picana.read().finish(); // {
                                         //Ok(guard) => guard.finish(),
                                         //_ => return -1,
